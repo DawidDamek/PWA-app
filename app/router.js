5a48c11c75d5dafda7b6cceeee68d6ba2dc9c485
Dawid Damek <dawid.damek98@gmail.com>
2022-10-31 09:53:10 +0100

    chore: remove unnecessary route

--- app/router.js
+++ app/router.js
@@ -8,6 +8,5 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('home', { path: '/' });
-  this.route('qr-scan', { path: 'scan' });
   this.route('maps');
 });
