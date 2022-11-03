322b3c83732e6f4a6b95c51b0785c93552ba45a7
Dawid Damek <dawid.damek98@gmail.com>
2022-11-03 16:19:05 +0100

    fix: fastboot window event listeners

--- app/pods/components/online-status/component.js
+++ app/pods/components/online-status/component.js
@@ -1,12 +1,33 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class OnlineStatusComponent extends Component {
-  @tracked isOnline = navigator.onLine;
+  @service fastboot;
+  @tracked isOnline;
 
   constructor() {
     super(...arguments);
-    window.addEventListener('online', () => (this.isOnline = true));
-    window.addEventListener('offline', () => (this.isOnline = false));
+    if (!this.fastboot.isFastBoot) {
+      this.fastboot = navigator.onLine;
+      window.addEventListener('online', this.onSetOnline);
+      window.addEventListener('offline', this.onSetOffline);
+    }
+  }
+  @action
+  onOnline() {
+    this.isOnline = true;
+  }
+
+  @action
+  onOffline() {
+    this.isOnline = false;
+  }
+
+  willDestroy() {
+    super.willDestroy(...arguments);
+    window.removeEventListener?.('online', this.onOnline);
+    window.removeEventListener?.('offline', this.onOffline);
   }
 }
