76fb1e996aa3a7136f607a3470eaa5fcf4b1f7e3
Dawid Damek <dawid.damek98@gmail.com>
2022-11-04 08:11:26 +0100

    wip: gyroscope component v3

--- app/pods/components/gyroscope/component.js
+++ app/pods/components/gyroscope/component.js
@@ -1,30 +1,27 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+
+// import { action } from '@ember/object';
 
 export default class GyroscopeComponent extends Component {
-  @tracked x;
-  @tracked y;
-  @tracked z;
-  @tracked gyroscope;
+  @service fastboot;
+  @tracked beta;
+  @tracked gamma;
+  @tracked alpha;
 
   constructor() {
     super(...arguments);
-
-    window.addEventListener(
-      'deviceorientation',
-      (event) => {
-        const rotateDegrees = event.alpha; // alpha: rotation around z-axis
-        const leftToRight = event.gamma; // gamma: left to right
-        const frontToBack = event.beta; // beta: front back motion
-
-        this.handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-      },
-      true
-    );
+    if (!this.fastboot.isFastBoot) {
+      window.addEventListener('deviceorientation', (event) => {
+        this.beta = event.beta;
+        this.gamma = event.gamma;
+        this.alpha = event.alpha;
+      });
+    }
   }
-  handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
-    this.x = frontToBack;
-    this.y = leftToRight;
-    this.z = rotateDegrees;
-  };
+
+  willDestroy() {
+    super.willDestroy(...arguments);
+  }
 }
